fc18bd4916068ae9e158c1ecfa58d9ffd186d9a1
Igor <zakatov0303@gmail.com>
2022-10-19 21:23:23 +0300

    Initialized firebase and redux store
    Created methods for initial authentication
    Inital styles for index

---
+++ src/firebase/index.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
+import { getAuth } from "@firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD0G-l8NTLltbp4ugvWfbQjj8ALKS8s3Lw",
+    authDomain: "book-store-83a29.firebaseapp.com",
+    databaseURL: "https://book-store-83a29-default-rtdb.firebaseio.com",
+    projectId: "book-store-83a29",
+    storageBucket: "book-store-83a29.appspot.com",
+    messagingSenderId: "142506729141",
+    appId: "1:142506729141:web:ce2023554ae88d8a6aa8fc"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app)
+export const auth = getAuth(app)
