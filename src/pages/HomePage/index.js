dbb92f2fea0cd9a64407141af52abed09fee1045
Igor <zakatov0303@gmail.com>
2022-10-30 17:54:43 +0300

    Created UserPage and HomePage

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import BookRow from '../../components/BookRow'
+import Loader from '../../components/Loader'
+import { fetchBooks, selectBooks } from '../../store/book'
+import './homePage.sass'
 
 function HomePage() {
+	const books = useSelector(selectBooks)
+	const dispatch = useDispatch()
+	const [isLoadingBooks, setIsLoadingBooks] = useState(true)
+
+	useEffect(() => {
+		if (!books.length) {
+			dispatch(fetchBooks()).then(() => {
+				setIsLoadingBooks(false)
+			})
+		} else {
+			setIsLoadingBooks(false)
+		}
+	}, [])
+
 	return (
-		<div>
-			<h1>Home page</h1>
+		<div className={ 'home-page-container' }>
+			<div className="block">
+				{ isLoadingBooks ? <Loader/> : <>
+					<div className="block-body">
+						<h3>Рекоммендации</h3>
+						<BookRow books={ books.filter(book => book.genre !== 'Программирование') }/>
+						<div className="divider"/>
+						<h3>Программирование</h3>
+						<BookRow books={ books.filter(book => book.genre === 'Программирование') } size={ 'large' }/>
+					</div>
+				</> }
+			</div>
 		</div>
 	)
 }
