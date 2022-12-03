89ae4c01a6a79c206279a204d30cd715c13a6dca
Igor <zakatov0303@gmail.com>
2022-12-03 17:11:32 +0300

    Added search field in my books page

--- src/pages/MyBooksPage/index.js
+++ src/pages/MyBooksPage/index.js
@@ -1,23 +1,31 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router'
 import CartPageItem from '../../components/Cart/CartPageItem'
+import Input from '../../components/Input'
 import List from '../../components/List'
 import { selectBooks } from '../../store/book'
 import { selectUser } from '../../store/user'
-import "./myBookPage.sass"
+import './myBookPage.sass'
 
 function MyBooksPage() {
 	const user = useSelector(selectUser)
 	const books = useSelector(selectBooks)
+	const [showBooks, setShowBooks] = useState(books)
 	const navigate = useNavigate()
+	const [searchQuery, setSearchQuery] = useState('')
 
 	useEffect(() => {
 		if (!user) navigate('')
 	}, [user])
 
+	useEffect(() => {
+		const sq = searchQuery.toLowerCase()
+		setShowBooks(books.filter(book => book.name.toLowerCase().includes(sq) || book.author.toLowerCase().includes(sq)))
+	}, [searchQuery])
+
 	return (
-		<div className={'mybooks-page'}>
+		<div className={ 'mybooks-page' }>
 			<div className="row">
 				<div className="col">
 					<div className="block">
@@ -25,11 +33,22 @@ function MyBooksPage() {
 							<h3>Мои книги</h3>
 						</div>
 						<div className="block-body">
+							<div className="search-field">
+								<label htmlFor={ 'mybook-search-input' }>Поиск:</label>
+								<Input
+									className={'search-input'}
+									id={ 'mybook-search-input' }
+									placeholder={ 'Введите название, автора...' }
+									value={searchQuery}
+									onChange={e => setSearchQuery(e.target.value)}
+								/>
+							</div>
+							<div className="divider"/>
 							<List
-								className={'wrap list-row jc-center list-gap-1'}
-								items={books}
-								getKey={book => book.id}
-								render={book => <CartPageItem user={user} book={book} removable={false}/>}
+								className={ 'wrap list-row jc-center list-gap-1' }
+								items={ showBooks }
+								getKey={ book => book.id }
+								render={ book => <CartPageItem user={ user } book={ book } removable={ false }/> }
 							/>
 						</div>
 					</div>
